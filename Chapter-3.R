bb64e36a54febd2aa4dab7b1303c3e136dac8375
krunal <krunalkk23@gmail.com>
2018-01-09 17:43:55 -0600

    Applied problem 8's solution

--- Chapter-3.R
+++ Chapter-3.R
@@ -89,5 +89,38 @@ vif(lm.fit)
 poly.fit = lm(medv ~ poly(lstat,2), data=Boston)
 summary(poly.fit)
 
+###############################################################################
+# Applied Problem 8
+###############################################################################
+a <- lm(mpg ~ horsepower, data = Auto)
+summary(a)
+predict(a, data.frame(horsepower=98), interval='prediction')
+# Comments
+# 1: Since the horsepower range is between 46-230 and the coefficient is < 1
+#    suggests that the relationship between mpg and horsepower is not that 
+#    strong, although the rlationship is . Also, the R^2 suggests that horsepower explains only about 60% of
+#    the variation in mpg.
+# Based on this model, a car with 98 horsepower will achieve 24.47 mpg on average
+# and this prediction with 95% confidence under repeated observations will lie 
+# between (14.81, 34.12)mpg.
+ggplot(Auto, aes(horsepower, mpg)) +
+  geom_point(shape='o') +
+  geom_abline(intercept = coef(a)[1], slope = coef(a)[2], color="blue",
+              size=1)
+# Diagnostics 
+# The resudual plot suggests a non-linear relationship
+# i.e. the variance is not constant over the domain of predictor variable.
+# Thus, the errors are not normally distributed with constant variance.
+resp <- data.frame(pred = predict(a), rst = rstudent(a))
+# Based on the delete 1 residuals, rstudent, there are multiple outliers
+ggplot(resp, aes(pred, rst)) +
+  geom_point() +
+  geom_abline(intercept = c(-2,2), slope=c(0,0), color='red')
+# There are multiple leverage points
+# Thus a linear model is not adequate to fit the data.
+plot(hatvalues(a))
+abline(h = 4/nrow(Auto), col="red")
+
+
 
 
