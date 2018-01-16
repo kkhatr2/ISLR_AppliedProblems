e8c73a84e295ffd1e7dd26601b433bc9110924f6
krunal <krunalkk23@gmail.com>
2018-01-15 22:44:15 -0600

    Applied problem 10 from ch-4

--- Chapter-4.R
+++ Chapter-4.R
@@ -2,6 +2,7 @@ library(ISLR)
 library(ggplot2)
 library(mvtnorm)
 library(MASS)
+library(class)
 
 # Lab Chapter 4
 glm.fits <- glm(Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume,
@@ -41,7 +42,6 @@ qda.pred <- predict(qda.fit, newdata = trainData, type="response")
 mean(qda.pred$class == dir.2005)
 
 # Using KNN
-library(class)
 set.seed(0)
 knn.fit <- knn(train = Smarket[train, c(2,3)], 
                test = Smarket[!train, c(2,3)],
@@ -79,5 +79,163 @@ for(i in 1:5){
   print(paste("Misclass: ", mean(test.y != caravan.knn)))
   print(paste("Correct Classification: ", ta[2,2]/sum(ta[2,])))
 }
+
+
+# Classification Applied Problems
+# Problem 10 using the Weekly dataset in ISLR
+summary(Weekly)
+
+# All the lag variables are shifted by the Lag number,
+# other than that they're same. Thus, their distribution is the same.
+ggplot(Weekly, aes(Lag1)) +
+  geom_density(fill = "blue", alpha=0.2) +
+  geom_density(aes(Lag2), fill="orange", alpha=0.2) +
+  geom_density(aes(Lag3), fill="green", alpha=0.2) +
+  geom_density(aes(Lag4), fill="grey5", alpha=0.2) +
+  geom_density(aes(Lag5), fill="yellow", alpha=0.2)
+
+# Distribution of the Direction Variable
+ggplot(Weekly, aes(Direction)) +
+  geom_histogram(aes(fill=Direction), stat = "count") +
+  scale_y_continuous(breaks=seq(0, 600, by=100)) +
+  theme(legend.position = "None") 
+
+# Distribution of variable Today
+ggplot(Weekly, aes(seq(1, nrow(Weekly), by=1),Today)) +
+  geom_point()
+
+# Distribution of Variable Today
+ggplot(Weekly, aes(Today)) +
+  geom_density(color="blue") +
+  geom_histogram(stat = "density", fill="blue", alpha=0.3)
+
+# Distribution of Year Variable
+ggplot(Weekly, aes(as.factor(Year))) +
+  geom_bar(fill = "grey", alpha = 0.5) +
+  theme(axis.text.x = element_text(angle = 90)) + 
+  labs(x = "Year", y = "Count", title = "Distribution of Year Variable")
+
+# Volume is very skewed to the right
+# Volume is measured in Billions
+ggplot(Weekly, aes(Volume)) +
+  geom_density(fill="Orange", color="pink", alpha = 0.5) +
+  labs(title="Distribution of Volume (in Billions)", y = "Density")
+
+
+# Using Logistic Regression to model Direction using all the predictors
+# except year
+weekly.glm <- glm(Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume,
+                  data = Weekly, family = binomial(link=logit))
+summary(weekly.glm)
+confint(weekly.glm)
+# Only Lag2 with a p-value of 0.02 and CI: (0.0062, 0.1117)
+# is significant
+# The Null Model Likelihood Ratio is 0.044 suggesting
+# that at least one of the estimates is != 0 and, Lag2 != 0
+dchisq(weekly.glm$null.deviance - weekly.glm$deviance, 
+       df = weekly.glm$df.null - weekly.glm$df.residual)
+
+weekly.pred <- ifelse(predict(weekly.glm, type="response") > 0.5, "Up", "Down")
+
+tr <- table(weekly.pred, Weekly$Direction)
+
+print(paste(cat("Misclassification Rate: ", 1 - round(sum(diag(tr))/sum(tr),4),
+            "\nThe Model is Misclassifying ", (1 - round(sum(diag(tr))/sum(tr),4))*100,
+            "% of times.\nIn this case there are a lot of false negatives.")))
+
+
+# Logistic Regression using only the most significant variable Lag2
+# Training the model on data until 2008 and testing the model
+# for data in 2009 and 2010
+train <- Weekly$Year < 2009   # Logical Vector
+train.data <- Weekly[train,]
+test.x <- Weekly[!train, -9]
+test.y <- Weekly$Direction[!train]
+
+
+weekly.glm1 <- glm(Direction ~ Lag2, data=train.data, family = binomial(link=logit))
+summary(weekly.glm1)
+weekly.test <- ifelse(predict(weekly.glm1, newdata = test.x, type="response") > 0.5, 
+                      "Up", "Down")
+
+tr1 <- table(weekly.test, test.y)
+
+
+print(paste(cat("Test Data Misclassification Rate: ", 
+                1 - round(sum(diag(tr1))/sum(tr1),4),
+                "\nThe Model is Misclassifying ", 
+                (1 - round(sum(diag(tr1))/sum(tr1),4))*100,
+                "% of times.\nIn this case also there are a lot of false negatives.")))
+
+# Using Linear Discriminant Analysis on the above test and train dataset
+weekly.lda = lda(Direction ~ Lag2, data = train.data)
+print(weekly.lda)
+
+lda.pred <- predict(weekly.lda, newdata = test.x, type="response")$class
+tr2 <- table(lda.pred, test.y)
+
+# The LDA Model has exactly the same misclassification rate as Logistic Reg
+print(paste(cat("Test Data Misclassification Rate: ", 
+                1 - round(sum(diag(tr2))/sum(tr2),4),
+                "\nThe Model is Misclassifying ", 
+                (1 - round(sum(diag(tr2))/sum(tr2),4))*100,
+                "% of times.\nIn this case also there are a lot of false negatives.")))
+
+# Using QDA on the train and test set.
+weekly.qda <- qda(Direction ~ Lag2, data = train.data)
+print(weekly.qda)
+
+qda.pred <- predict(weekly.qda, newdata = test.x)$class
+
+tr3 <- table(qda.pred, test.y)
+
+print(cat("The QDA Model does not predict that the market will go down
+          at all and, it is wrong 43 out of 104 times.\nAll the error is
+          from false negatives and the misclassification rate is: ",
+          1 - sum(diag(tr3)/sum(tr3))))
+
+# Using KNN method for classification
+res <- data.frame(misclass = rep(NA, 5), 
+                  method = c("knn(1)", "knn(3)", "knn(5)", "knn(7)", "knn(9)"))
+
+for( i in seq(1, 9, by=2)){
+  set.seed(i)
+  weekly.knn <- knn(train = train.data[,-9], 
+                    test = test.x, 
+                    cl = train.data$Direction,
+                    k = i)
+  tr <- table(weekly.knn, test.y)
+  res[which(res$method == paste("knn(",i,")", sep="")),"misclass"] = 
+    round(1 - sum(diag(tr))/sum(tr), 4)
+}
+
+res <- rbind(res, data.frame(misclass = c(1 - round(sum(diag(tr3))/sum(tr3), 4),
+                                          1 - round(sum(diag(tr2))/sum(tr2), 4),
+                                          1 - round(sum(diag(tr1))/sum(tr1),4)),
+                             method = c("QDA",
+                                        "LDA",
+                                        "Logistic")))
+
+ggplot(res, aes(reorder(method, -misclass), misclass)) +
+  geom_bar(aes(fill=method), stat="identity") +
+  geom_text(aes(label = misclass), vjust = -0.6, size=4) +
+  labs(x = "Method", y = "Misclassification Rate",
+       title="Misclass Rate of Different models for Classification") +
+  theme(legend.position = "None")
+
+
+
+
+
+
+
+
+
+
+
+               
+               
+               
+
 
 
