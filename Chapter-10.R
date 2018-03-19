5ebf9ef1f0e06e8d4a6b98710c66a55042e576cf
krunal <krunalkk23@gmail.com>
2018-03-19 16:38:18 -0500

    Created Ch-10 File

---
+++ Chapter-10.R
@@ -0,0 +1,8 @@
+library(ISLR)
+library(ggplot2)
+
+####################################################################################
+###### Problem 7
+# Similarity of Eucledian distance and correlations of standardized variables.
+####################################################################################
+
