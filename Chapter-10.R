b1ff98a55a84a13e88d43c59d25fdf5020aaa7bd
krunal <krunalkk23@gmail.com>
2018-03-19 17:34:55 -0500

    Problem 7 complete

--- Chapter-10.R
+++ Chapter-10.R
@@ -5,4 +5,13 @@ library(ggplot2)
 ###### Problem 7
 # Similarity of Eucledian distance and correlations of standardized variables.
 ####################################################################################
+arrests.sc = scale(USArrests)
+arrests.dist = 1 / dist(arrests.sc)^2
+arrests.corr = as.dist(1 - cor(t(arrests.sc)))
 
+lo = round(arrests.dist - arrests.corr, 1)
+table(lo)
+
+# Based on the table all the differences are between (-1.9, 0.8)
+# So, yes these the measures of distance and correlation are about the same as
+# the proportional square.
