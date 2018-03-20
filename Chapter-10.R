f64b9a8b926fbe0b78ac188503f7d8dd22b951c9
krunal <krunalkk23@gmail.com>
2018-03-19 22:43:35 -0500

    Completed problem 8

--- Chapter-10.R
+++ Chapter-10.R
@@ -15,3 +15,23 @@ table(lo)
 # Based on the table all the differences are between (-1.9, 0.8)
 # So, yes these the measures of distance and correlation are about the same as
 # the proportional square.
+
+
+##################################################################################
+###### Problem 8
+# Computing Percent Variance Explained (PVE) of principal components
+##################################################################################
+# Using the simple approach by using std. dev. from using prcomp
+arrests.pca = prcomp(arrests.sc, center = F, scale. = F)
+pca.pve = arrests.pca$sdev^2 / sum(arrests.pca$sdev^2)
+plot(pca.pve, type = "b", col="red", ylim = c(0,1))
+lines(cumsum(pca.pve), type = "b", col="blue")
+
+# Using the formula
+# Sum each column of the principal components and square them to get the numerator
+den.sum = sum(apply(arrests.sc, MARGIN = 2, FUN = function(x) sum(x^2)))
+num.sum = apply(arrests.pca$x, MARGIN = 2, FUN = function(x) sum(x^2))
+
+pve = num.sum/den.sum
+plot(pve, type = "b", col = "red", ylim = c(0,1))
+lines(cumsum(pve), col = "blue")
